ea62158f7158aa581f077802a0ec7ef33164e11d
HazardChem <pthom44@live.com.au>
2022-05-19 05:54:59 +1000

    Changed the direction of diff.

--- xkb/ppkb_settings_test.sh
+++ xkb/ppkb_settings_test.sh
@@ -7,9 +7,9 @@ echo "Outputting your current xkb settings"
 xmodmap -pm
 echo ""
 echo "Comparing files from this repo to your local files, will do this recursively."
-echo "Differences preceeded with < are in the repo"
-echo "Differences preceeded with > are in your local files"
+echo "Differences preceeded with < are in your local files"
+echo "Differences preceeded with > are in the repo"
 echo "Files listed with 'Only in' don't have any differences"
 echo ""
-diff -r ./rules /usr/share/X11/xkb/rules
-diff -r ./symbols /usr/share/X11/xkb/symbols
+diff -r /usr/share/X11/xkb/rules ./rules
+diff -r /usr/share/X11/xkb/symbols ./symbols
