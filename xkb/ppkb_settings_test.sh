c1c4de1a5cdbec46161de64e7308fc7ecd81e5ab
HazardChem <pthom44@live.com.au>
2022-05-18 21:42:18 +1000

    Added more tests to help narrow down issues.

--- xkb/ppkb_settings_test.sh
+++ xkb/ppkb_settings_test.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+echo "Outputting your current xkb settings"
+xmodmap -pm
+echo ""
 echo "Comparing files from this repo to your local files, will do this recursively."
 echo "Differences preceeded with < are in the repo"
 echo "Differences preceeded with > are in your local files"
 echo "Files listed with 'Only in' don't have any differences"
 echo ""
-diff -r --color ./rules /usr/share/X11/xkb/rules
-diff -r --color ./symbols /usr/share/X11/xkb/symbols
+diff -r ./rules /usr/share/X11/xkb/rules
+diff -r ./symbols /usr/share/X11/xkb/symbols
