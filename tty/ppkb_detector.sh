5acdcec32ef311fe18b1042351b4266c41d79070
HazardChem <hazardchem@noreply.codeberg.org>
2022-06-02 10:53:00 +0200

    Updated to reference upstream merge

--- tty/ppkb_detector.sh
+++ tty/ppkb_detector.sh
@@ -7,6 +7,6 @@ then
     loadkeys us
     echo 0 > /sys/class/graphics/fbcon/rotate
 else
-    loadkeys /usr/local/share/kbd/keymap/pine/ppkb.map
+    loadkeys /usr/share/kbd/keymap/pine/en.map
     echo 1 > /sys/class/graphics/fbcon/rotate
 fi
