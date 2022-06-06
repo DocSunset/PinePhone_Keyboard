a340f951dc24edaa9b401c2a53ade780e55e4d87
HazardChem <hazardchem@noreply.codeberg.org>
2022-06-06 03:26:41 +0200

    Update 'tty/ppkb_detector.sh'

--- tty/ppkb_detector.sh
+++ tty/ppkb_detector.sh
@@ -7,6 +7,6 @@ then
     loadkeys us
     echo 0 > /sys/class/graphics/fbcon/rotate
 else
-    loadkeys /usr/share/kbd/keymap/pine/en.map
+    loadkeys /usr/share/kbd/keymap/pine/en.map.gz
     echo 1 > /sys/class/graphics/fbcon/rotate
 fi
